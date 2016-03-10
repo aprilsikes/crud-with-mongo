3a838410b9af731c25fdf03d666f1de82efbb077
april sikes <aprilsikes@gmail.com>
2016-03-09 19:48:06 -0700

    create with mongo

---
+++ routes/restaurants.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var router = express.Router();
+var app = express();
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/restaurants_development');
+
+var Schema = mongoose.Schema;
+
+var Restaurants = new Schema({
+  name: String,
+  location: String,
+  created_at: Date,
+  updated_at: Date
+});
+
+var RestaurantsModel = mongoose.model('Restaurants', Restaurants);
+
+router.get('/', function(req, res, next){
+	return RestaurantsModel.find(function (restaurants) {
+			res.render('restaurants/restIndex', {restaurants: restaurants});
+	});
+});
+
+router.get('/new', function(req, res, next){
+	res.render('restaurants/restNew')
+});
+
+router.post('/', function (req, res, next) {
+  restaurant = new RestaurantsModel({
+    name: req.body.name,
+    location: req.body.location
+  });
+
+  restaurant.save(function () {
+    res.redirect(301, '/restaurants');
+  });
+
+});
+
+module.exports = router;
